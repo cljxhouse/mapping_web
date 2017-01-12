72873f90a543f6cb99b7cf55e246ef92ec9a2313
wujixiong <wujx@cn.ibm.com>
2017-01-12 22:24:56 +0800

    add submit and sync

--- map_app/map_app/mappingtool.py
+++ map_app/map_app/mappingtool.py
@@ -126,12 +126,19 @@ def ShowMapping():
                            map_sql=map_sql)
 
 
-
+#显示Mapping列表
+#2017-1-6 jxk update
 @myapp.route('/map/view', methods=['GET', 'POST'])
 def ShowMappingList():
+    print "ShowMappingList"
     conn = getDB()
     cur = conn.cursor()
     filter = request.args.get('keyword', '')
+    tab_name = request.args.get('target_table','')
+    tag= request.args.get('tag','')
+    print tab_name
+    #print tag
+
     s1 = ''
     if filter != "":
         s1 = "Where upper(t.map_name) like '%" + filter.upper() + "%'"
@@ -142,7 +149,7 @@ def ShowMappingList():
     cur.execute(sql_str)
     maplist = []
     allmaps = cur.fetchall()
-    cur.close()
+
     # print cur.description
     for onerow in allmaps:
         dic = {}
@@ -152,19 +159,58 @@ def ShowMappingList():
         dic['last_save_time'] = onerow[3]
         dic['mapping_status'] = onerow[4]
         maplist.append(dic)
-    return render_template('list_maps.html', maplist=maplist)
+
+    #list tables
+    sql_str ="select distinct tab_name from t_column where repo_id = 'RDM' order by tab_name"
+    cur.execute(sql_str)
+    tables=[]
+    alltables = cur.fetchall()
+
+    for onerow in alltables:
+        dic = {}
+        dic['tab_name'] = onerow[0]
+        tables.append(dic)
+
+    #list columns
+    col_list = []
+    if tab_name != "":
+        print "get cols"
+        sql_str = "select * from t_column where repo_id = 'RDM'  and tab_name= '%s' order by col_position"%(tab_name)
+        cur.execute(sql_str)
+        all_cols = cur.fetchall()
+        for onerow in all_cols:
+            dic = {}
+            dic['col_position'] = onerow[5]
+            dic['col_cn_name'] = onerow[6]
+            dic['col_name'] = onerow[2]
+            dic['dtype'] = onerow[4]
+            dic['isprimary'] = onerow[8]
+            dic['col_desc'] = onerow[3]
+            dic['column_id'] = onerow[9]
+            dic['tab_ch_name'] = onerow[1]
+            col_list.append(dic)
+        cur.close()
+        return render_template('list_maps.html', maplist=maplist, tables=tables, col_list=col_list, tab_name=tab_name, tag=tag)
+
+    return render_template('list_maps.html', maplist=maplist, tables=tables, tag=tag)
 
 
 # 删除映射
+# 2017-1-5 jxk update
 @myapp.route('/map/delete', methods=['GET', 'POST'])
 def DeleteMapping():
     map_name = request.args.get('map_name', '')
     if map_name != "":
-        with getDB() as conn:
-            with conn.cursor() as curr:
-                curr.execute("delete from t_col_map where map_name ='" + map_name + "'");
-                curr.execute("delete from t_src_table where map_name ='" + map_name + "'");
-                curr.execute("delete from t_tab_map where map_name ='" + map_name + "'");
+        #with getDB() as conn:
+        conn = getDB();
+        curr = conn.cursor();
+        #with conn.cursor() as curr:
+        #print "delete from t_col_map where map_name ='" + map_name + "'"
+        curr.execute("delete from t_col_map where map_name ='" + map_name + "'");
+        curr.execute("delete from t_src_table where map_name ='" + map_name + "'");
+        curr.execute("delete from t_tab_map where map_name ='" + map_name + "'");
+        conn.commit();
+        conn.close();
     return redirect('/')
 
 @myapp.route('/map/update_status', methods=['GET', 'POST'])
@@ -173,6 +219,7 @@ def UpdateMappingStatusView():
     p_status = request.args.get('status', '')
     return render_template("map_update_status.html",map_name=p_map_name,status=p_status)
 
+# 2017-1-5 jxk update
 @myapp.route('/map/update_status_action', methods=['GET', 'POST'])
 def UpdateMappingStatusAction():
     p_map_name = request.form['map_name']
@@ -180,7 +227,10 @@ def UpdateMappingStatusAction():
     conn = getDB()
     curr = conn.cursor()
     try :
-        curr.execute("update t_tab_map set mapping_status=%s where map_name=%s",(p_status,p_map_name))
+        #print "-------------"
+        #print ("update t_tab_map set mapping_status='%s' where map_name='%s'" %(p_status, p_map_name))
+        curr.execute("update t_tab_map set mapping_status='%s' where map_name='%s'"%(p_status,p_map_name))
+
         conn.commit()
     finally:
         return redirect("/map/view")
@@ -263,3 +313,89 @@ def MapSourceUpdate():
                          where map_name=%s and tab_order=%s ''', [j1, j2, f1, f2,c1,map_name, tab_order]
                          )
     return redirect(url_for("ShowMapping", map_name=map_name))
+
+
+# 删除一个字段
+# 2017-1-6 jxk update
+@myapp.route('/repo/deleteRepoRow', methods=['GET', 'POST'])
+def DeleteRepoRow():
+    #print "DeleteRepoRow"
+    tab_name = request.args.get('tab_name', '')
+    col_name = request.args.get('col_name', '')
+    column_id = request.args.get('column_id','')
+    #print tab_name
+    #print col_name
+    if tab_name != "":
+        #with getDB() as conn:
+        conn = getDB();
+        curr = conn.cursor();
+        #with conn.cursor() as curr:
+        #print "delete from t_col_map where map_name ='" + map_name + "'"
+        curr.execute("delete from t_column where column_id='%s' "%(column_id))
+        conn.commit();
+        conn.close();
+    return redirect(url_for("ShowMappingList",tag="table",target_table=tab_name))
+
+#保存一个字段
+#2017-1-7 jxk
+@myapp.route('/repo/saveRepoRow', methods=['GET', 'POST'])
+def SaveRepoRow():
+    tab_name = request.args.get('tab_name', '')
+    tab_ch_name = request.args.get('tab_ch_name','')
+    col_name = request.args.get('col_name', '')
+    column_id = request.args.get('column_id', '')
+    col_desc = request.args.get('col_desc','')
+    dtype = request.args.get('dtype','')
+    col_position = request.args.get('col_position','')
+    col_ch_name = request.args.get('col_ch_name','')
+    isprimary = request.args.get('isprimary','')
+    is_necessary = request.args.get('is_necessary','')
+
+    print "column_id:"+column_id
+    conn = getDB()
+    curr = conn.cursor()
+
+    if column_id  =="":
+        #新增字段
+
+        curr.execute("insert into t_column(repo_id,tab_ch_name,col_name,col_desc,dtype,col_position,col_ch_name,tab_name,isprimary,is_necessary)"
+                     " values('RDM','%s','%s','%s','%s','%s','%s','%s','%s','%s')"%(tab_ch_name,col_name,col_desc,dtype,col_position,col_ch_name,tab_name,isprimary,is_necessary))
+    else:
+        #更新字段
+        print ("delete from t_column where column_id='%s'"%(column_id))
+        curr.execute("delete from t_column where column_id='%s'"%(column_id))
+        print ("insert into t_column values('RDM','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',null)" % (
+        tab_ch_name, col_name, col_desc, dtype, col_position, col_ch_name, tab_name, isprimary, column_id, is_necessary))
+        curr.execute("insert into t_column values('RDM','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',null)" % (
+        tab_ch_name, col_name, col_desc, dtype, col_position, col_ch_name, tab_name, isprimary, column_id, is_necessary))
+
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("ShowMappingList",tag="table",target_table=tab_name))
+
+#修改字段
+#2017-1-7 jxk
+@myapp.route('/repo/editRepoColumn', methods=['GET', 'POST'])
+def RepoEditRepoColumnPage():
+    column_id = request.args.get('column_id', '')
+    tab_name = request.args.get('tab_name', '')
+    tab_ch_name = request.args.get('tab_ch_name', '')
+
+    #print "column_id:"+column_id
+    #print "tab_name:" + tab_name
+
+    #curr.execute("select * from t_column where column_id=%s" %(column_id))
+    column_info={ }
+    if column_id !="":
+        conn = getDB();
+        curr = conn.cursor();
+        curr.execute("select * from t_column where column_id="+column_id)
+        r = curr.fetchone()
+        column_info = {'tab_name': tab_name, 'column_id': column_id, 'col_name': r[2], 'col_desc': r[3], 'dtype': r[4], 'col_position': r[5], 'col_ch_name': r[6], 'isprimary': r[8], 'is_necessary': r[10],'tab_ch_name':r[1]}
+        conn.commit()
+        conn.close()
+    else:
+        column_info = {'tab_name': tab_name, 'column_id': "", 'col_name': "", 'col_desc': "", 'dtype': "",'col_position': "", 'col_ch_name': "", 'isprimary': "", 'is_necessary': "",'tab_ch_name':tab_ch_name}
+
+    return render_template('show_repo_columns.html', column_info=column_info)
